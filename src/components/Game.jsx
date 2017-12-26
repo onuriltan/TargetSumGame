8e3b058b69fea525dc21f6cf197286311026391a
Onur ILTAN <onuriltan@Onurs-iMac.local>
2017-12-26 13:12:37 +0300

    initial setup

---
+++ src/components/Game.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import '../components/Game.css';
+
+class Game extends Component{
+    render() {
+        return (
+          <div className="game">
+            <div className="help">
+              Pick 4 numbers that sum to the target in 15 seconds
+            </div>
+            <div className="target">42</div>
+            <div className="challenge-numbers">
+              <div className="number">8</div>
+              <div className="number">5</div>
+              <div className="number">12</div>
+            </div>
+              <div className="challenge-numbers">
+              <div className="number">13</div>
+              <div className="number">5</div>
+              <div className="number">16</div>
+            </div>
+            <div className="footer">
+              <div className="timer-value">15</div>
+              <button>Start</button>
+            </div>
+          </div>
+        );
+      }
+}
+
+export default Game;
