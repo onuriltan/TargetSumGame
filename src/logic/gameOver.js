c326087af0729c12b0e20cbbefeef32762877fc1
onuriltan <onur.iltan@altpro.com.tr>
2018-01-12 17:39:35 +0300

    when game over do not change initial sum

--- src/logic/gameOver.js
+++ src/logic/gameOver.js
@@ -5,7 +5,6 @@ export default function gameOver(initialState) {
     initialState.resetButtonDisabled = true;
     initialState.targetNumber = 'GAME OVER';
     initialState.gameState = 'LOST';
-    initialState.initialSum = 0;
 
     return initialState;
   }
