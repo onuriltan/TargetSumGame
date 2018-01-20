e660e8591eb38807182a772f50c85a7ffaac5e2f
onuriltan <onur.iltan@altpro.com.tr>
2018-01-20 11:10:30 +0300

    redux devtools excluded in production

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,22 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
 
 export default function configureStore() {
-    return createStore(
-        rootReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-        applyMiddleware(thunk)
-    );
+
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') { //https://stackoverflow.com/a/35470995
+        console.log(process.env.NODE_ENV);
+
+        return createStore(
+            rootReducer,
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+            applyMiddleware(thunk)
+        );
+    } 
+    else {
+        return createStore(
+            rootReducer,
+            applyMiddleware(thunk)
+        );
+    }
 }
