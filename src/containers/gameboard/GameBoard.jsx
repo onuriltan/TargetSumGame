a74df0c21603ff1600b5daafe14508670a7ad67d
onuriltan <onur.iltan@altpro.com.tr>
2018-05-02 20:56:23 +0300

    explanation spelling correction

--- src/containers/gameboard/GameBoard.jsx
+++ src/containers/gameboard/GameBoard.jsx
@@ -33,7 +33,7 @@ class GameBoard extends Component {
 
                 <div className="header">
                     <div className="win-count">Win Count: {this.state.initialState.timesOfPlay}</div>
-                    <div className="explanation">Sum the numbers to react the target</div>
+                    <div className="explanation">Sum the numbers to reach the target</div>
                 </div>
                 <div className="target">{this.state.initialState.targetNumber}</div>
                 <div className="initial-sum">Initial sum : {this.state.initialState.initialSum}</div>
